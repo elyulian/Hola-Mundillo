95a63a8a3eb236e1efb06c2849be4de333579fbf
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2021-03-12 09:06:00 -0500

    Fibo

--- consola/Program.cs
+++ consola/Program.cs
@@ -6,8 +6,17 @@ namespace consola
     {
         static void Main(string[] args)
         {
+            int aux = 1, fib = 0;
             Console.WriteLine("Hola Dani ");
             Console.WriteLine("uyuyuyuyuy");
+        
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(fib);
+
+                aux += fib;
+                fib = aux + fib;
+            }
         }
     }
 }
