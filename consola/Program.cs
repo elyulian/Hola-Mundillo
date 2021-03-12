bd7995d7667db214ed07c2185018908636d87a9b
elyulian <julianherrera0206@gmail.com>
2021-03-12 07:46:36 -0500

    Agregar archivos de proyecto.

---
+++ consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Dani ");
+            Console.WriteLine("uyuyuyuyuy");
+        }
+    }
+}
